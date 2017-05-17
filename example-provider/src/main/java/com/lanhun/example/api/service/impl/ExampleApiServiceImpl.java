ff0bdd1a69cda4f2d37784c7ae1065845610b12e
李淼淼 <445052471@qq.com>
2017-05-17 11:33:26 +0800

    依赖调整

--- example-provider/src/main/java/com/lanhun/example/api/service/impl/ExampleApiServiceImpl.java
+++ example-provider/src/main/java/com/lanhun/example/api/service/impl/ExampleApiServiceImpl.java
@@ -1,11 +1,15 @@
 package com.lanhun.example.api.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.lanhun.example.api.model.ExampleModel;
 import com.lanhun.example.api.service.ExampleApiService;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Administrator on 2017/5/16.
  */
+@Service
+@Component
 public class ExampleApiServiceImpl implements ExampleApiService {
 
     public ExampleModel example(String name) {
