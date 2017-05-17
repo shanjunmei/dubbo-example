b0e18802f252bda909cade8d895c84df8b809df1
李淼淼 <445052471@qq.com>
2017-05-17 11:58:07 +0800

    配置优化

--- example-core/src/main/java/com/lanhun/example/service/impl/ExampleServiceImpl.java
+++ example-core/src/main/java/com/lanhun/example/service/impl/ExampleServiceImpl.java
@@ -2,13 +2,14 @@ package com.lanhun.example.service.impl;
 
 import com.lanhun.example.api.model.ExampleModel;
 import com.lanhun.example.service.ExampleService;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 /**
  * Created by Administrator on 2017/5/17.
  */
-
+@Service
 public class ExampleServiceImpl  implements ExampleService{
 
     public ExampleModel example(String name) {
--- example-web/src/main/java/com/lanhun/example/web/ExampleController.java
+++ example-web/src/main/java/com/lanhun/example/web/ExampleController.java
@@ -1,7 +1,24 @@
 package com.lanhun.example.web;
 
+import com.lanhun.example.api.model.ExampleModel;
+import com.lanhun.example.api.service.ExampleApiService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
 /**
  * Created by Administrator on 2017/5/16.
  */
+@RestController
+@RequestMapping("example")
 public class ExampleController {
+
+    @Resource
+    private ExampleApiService service;
+
+    @RequestMapping("hello")
+    public ExampleModel example(String name){
+        return service.example(name);
+    }
 }
