3e5314843232d66b5d75545141d8e70514822051
李淼淼 <445052471@qq.com>
2017-05-17 15:24:08 +0800

    服务初始化方式调整

--- example-provider/src/main/java/com/lanhun/example/api/service/impl/ExampleApiServiceImpl.java
+++ example-provider/src/main/java/com/lanhun/example/api/service/impl/ExampleApiServiceImpl.java
@@ -1,6 +1,5 @@
 package com.lanhun.example.api.service.impl;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.lanhun.example.api.model.ExampleModel;
 import com.lanhun.example.api.service.ExampleApiService;
 import com.lanhun.example.service.ExampleService;
@@ -11,7 +10,7 @@ import javax.annotation.Resource;
 /**
  * Created by Administrator on 2017/5/16.
  */
-@Service
+
 @Component
 public class ExampleApiServiceImpl implements ExampleApiService {
 
